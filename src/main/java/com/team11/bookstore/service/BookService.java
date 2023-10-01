97e579c1fd82c7fcde28efa840c3c1666932a6f4
Ernesto Gonzalez <ggoku02@gmail.com>
2023-10-01 18:39:05 -0500

    Added more example code to BookService

--- src/main/java/com/team11/bookstore/service/BookService.java
+++ src/main/java/com/team11/bookstore/service/BookService.java
@@ -9,10 +9,21 @@ import java.util.List;
 @Service
 public class BookService {
 
+    // MUST have - replace repository name with pertinent one
     @Autowired
     private BookRepository bookRepository;
 
     public List<M_Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    public void deleteAllBooks(){
+        bookRepository.deleteAll();
+    }
+
+
+    public List<M_Book> getBooksByGenre(String genre) {
+        // filter books by genre and return list of books of a specific genre
+        return null;
     }
 }
