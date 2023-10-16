b5d6cd669864ac4718d1c871888ff9f7a1a5277c
umikami <ggoku02@gmail.com>
2023-10-16 01:53:52 -0400

    added query method to retrieve shopping cart by user id from database

--- src/main/java/com/team11/bookstore/repository/ShoppingCartRepository.java
+++ src/main/java/com/team11/bookstore/repository/ShoppingCartRepository.java
@@ -4,7 +4,11 @@ import com.team11.bookstore.model.M_ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<M_ShoppingCart, Integer> {
     // Custom query methods
+    Optional<M_ShoppingCart> findByUserID(Integer userID);
+
 }
