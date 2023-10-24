68ac18a2172827e9726f7618caf60574171d2fa6
camgloria <cam_gloria@yahoo.com>
2023-10-23 22:32:42 -0400

    Added fix to prevent creation of new user with a taken username.

--- src/main/java/com/team11/bookstore/controller/profile/UserController.java
+++ src/main/java/com/team11/bookstore/controller/profile/UserController.java
@@ -5,6 +5,7 @@ import com.team11.bookstore.model.M_User;
 import com.team11.bookstore.service.BookService;
 import com.team11.bookstore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,8 +26,18 @@ public class UserController {
 
     @PostMapping(path = "/signup", produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void signup(@RequestBody M_User newUser) {
-        userService.saveUser(newUser);
+    public ResponseEntity<String> signup(@RequestBody M_User newUser) {
+        M_User user = userService.getUser(newUser.getUsername());
+        if (user == null) {
+            if (newUser.getUsername() == null || newUser.getPassword() == null) {
+                return new ResponseEntity<>("Username and password are required.", HttpStatus.BAD_REQUEST);
+            }
+            userService.saveUser(newUser);
+            return new ResponseEntity<>("User created successfully.", HttpStatus.CREATED);
+        }
+        else {
+            return new ResponseEntity<>("Username is taken.", HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/{username}")
