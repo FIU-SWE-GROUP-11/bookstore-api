0d312351665949fad534b6ba520300d5c9b8772d
Christina Gonzalez <144561008+ch13gonzalez@users.noreply.github.com>
2023-10-29 18:17:33 -0400

    Implemented find books by rating and apply discount by publisher functionalities

--- src/main/java/com/team11/bookstore/controller/browsingEtSorting/BookController.java
+++ src/main/java/com/team11/bookstore/controller/browsingEtSorting/BookController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -32,5 +34,23 @@ public class BookController {
         return bookService.findByCopiesSold(copiesSold);
     }
 
+    @GetMapping("/by-rating/{rating}")
+    public List<M_Book> findBooksByRating(@PathVariable Integer rating) {
+        return bookService.findBooksByRating(rating);
+    }
+
+    @PatchMapping("/apply-discount/{publisher}/{discountPercent}")
+    public void applyDiscountToBooksByPublisher(
+            @PathVariable String publisher,
+            @PathVariable BigDecimal discountPercent) {
+        bookService.applyDiscountToBooksByPublisher(publisher, discountPercent);
+    }
+
+    @PatchMapping("/remove-discount/{publisher}/{discountPercent}")
+    public void removeDiscountFromBooksByPublisher(
+            @PathVariable String publisher,
+            @PathVariable BigDecimal discountPercent) {
+        bookService.removeDiscountFromBooksByPublisher(publisher, discountPercent);
+    }
     // Other mappings for book browsing operations
 }
--- src/main/java/com/team11/bookstore/repository/BookRepository.java
+++ src/main/java/com/team11/bookstore/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.team11.bookstore.repository;
 
 import com.team11.bookstore.model.M_Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -10,4 +12,10 @@ public interface BookRepository extends JpaRepository<M_Book, Integer> {
     // Custom query methods
     List<M_Book> findByGenre(String genre);
     List<M_Book> findByCopiesSold(Integer copiesSold);
+    List<M_Book> findByPublisher(String publisher);
+
+    @Query("SELECT b FROM M_Book b WHERE b.bookID IN (SELECT r.book.bookID FROM M_Ratings r WHERE r.ratingValue >= :rating)")
+    List<M_Book> findBooksByRating(@Param("rating") Integer rating);
+
+
 }
