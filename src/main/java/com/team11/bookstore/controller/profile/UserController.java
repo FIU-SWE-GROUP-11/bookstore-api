a96ca1a4ceffcd2a6da3dc623d7c593083ce970d
camgloria <cam_gloria@yahoo.com>
2023-11-09 00:50:33 -0500

    Added fixes regarding case sensitivity and other minor changes.

--- src/main/java/com/team11/bookstore/controller/profile/UserController.java
+++ src/main/java/com/team11/bookstore/controller/profile/UserController.java
@@ -26,45 +26,37 @@ public class UserController {
     @PostMapping(path = "/signup", produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> signup(@RequestBody M_User newUser) {
-        M_User user = userService.getUser(newUser.getUsername());
-        if (user == null) {
+        if (!userService.userExistsByUsername(newUser.getUsername(), false)) {
             if (newUser.getUsername() == null || newUser.getPassword() == null) {
                 return new ResponseEntity<>("Username and password are required.", HttpStatus.BAD_REQUEST);
             }
             userService.saveUser(newUser);
             return new ResponseEntity<>("User created successfully.", HttpStatus.CREATED);
         }
-        else {
-            return new ResponseEntity<>("Username is taken.", HttpStatus.BAD_REQUEST);
-        }
+        String errorMessage = "Username \"" + newUser.getUsername() + "\" is taken.";
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping("/{username}")
     public ResponseEntity<?> getUserByUsername(@PathVariable String username) {
-        M_User user = userService.getUser(username);
-        if (user != null) {
-            return new ResponseEntity<>(user, HttpStatus.OK);
+        if (userService.userExistsByUsername(username, false)) {
+            return new ResponseEntity<>(userService.getUser(username, false), HttpStatus.OK);
         }
-        else {
-            String errorMessage = "User \"" + username + "\" does not exist.";
-            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
-        }
+        String errorMessage = "User \"" + username + "\" does not exist.";
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    @PostMapping(path = "/{username}/add-credit-card", produces = MediaType.APPLICATION_JSON_VALUE,
+    @PostMapping(path = "/add-credit-card/{username}", produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> addCreditCard(@PathVariable String username, @RequestBody M_CreditCard newCard) {
-        M_User user = userService.getUser(username);
-        if (user != null) {
-            newCard.setUser(user);
+        if (userService.userExistsByUsername(username, false)) {
+            newCard.setUser(userService.getUser(username, false));
             userService.saveCreditCard(newCard);
-            String successMessage = "Successfully added credit card for \"" + username + "\".";
+            String successMessage = "Successfully added credit card for \"" + userService.getUser(username, false).getUsername() + "\".";
             return new ResponseEntity<>(successMessage, HttpStatus.CREATED);
         }
-        else {
-            String errorMessage = "User \"" + username + "\" does not exist.";
-            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
-        }
+        String errorMessage = "User \"" + username + "\" does not exist.";
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
 
